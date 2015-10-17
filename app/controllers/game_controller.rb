0f6067a9b157daffaf5cb326850e10eacb6e12a9
jordi92500 <jordi92500@hotmail.fr>
2015-10-17 18:17:48 -0500

    Definition of computer move

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,13 +1,28 @@
 class GameController < ApplicationController
 
   def rock
-
+    computer_play
   end
 
   def paper
+    computer_play
   end
 
   def scissors
+    computer_play
+  end
+
+  private
+
+  def computer_play
+    case @rand_num = rand(1..3)
+      when 1
+        @computer_play = "Rock"
+      when 2
+        @computer_play = "Paper"
+      when 3
+        @computer_play = "Scissors"
+    end
   end
 
 end
