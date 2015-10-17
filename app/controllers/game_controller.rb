2288f37691722244d66972599b84710b064ec836
jordi92500 <jordi92500@hotmail.fr>
2015-10-17 18:28:01 -0500

    add of Player lost/win

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,20 +2,47 @@ class GameController < ApplicationController
 
   def rock
     computer_play
+    @player_move = "Rock"
+
+    if computer_play == "rock"
+      @result = "tied"
+    elsif computer_play == "Paper"
+      @result = "won"
+    else
+      @result = "lost"
+    end
   end
 
   def paper
     computer_play
+    @player_move = "Paper"
+
+    if computer_play == "rock"
+      @result = "won"
+    elsif computer_play == "Paper"
+      @result = "tied"
+    else
+      @result = "lost"
+    end
   end
 
   def scissors
     computer_play
+    @player_move = "Scissors"
+
+    if computer_play == "rock"
+      @result = "lost"
+    elsif computer_play == "Paper"
+      @result = "won"
+    else
+      @result = "tied"
+    end
   end
 
   private
 
   def computer_play
-    case @rand_num = rand(1..3)
+    case rand(1..3)
       when 1
         @computer_play = "Rock"
       when 2
@@ -24,5 +51,4 @@ class GameController < ApplicationController
         @computer_play = "Scissors"
     end
   end
-
 end
