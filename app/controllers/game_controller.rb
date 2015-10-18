455b6dbf04113945e28e3cd13b514964db3e8a75
jordi92500 <jordi92500@hotmail.fr>
2015-10-17 19:09:30 -0500

    Styling + Use of partial/layout
    To keep the code from being written multiple times

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,42 +1,48 @@
 class GameController < ApplicationController
 
   def rock
-    computer_play
     @player_move = "Rock"
 
-    if computer_play == "rock"
-      @result = "tied"
-    elsif computer_play == "Paper"
-      @result = "won"
-    else
-      @result = "lost"
+    case computer_play
+      when "Rock"
+        @result = "tied"
+      when "Paper"
+        @result = "lost"
+      when "Scissors"
+        @result = "won"
     end
+
+    result_label(@result)
   end
 
   def paper
-    computer_play
     @player_move = "Paper"
 
-    if computer_play == "rock"
-      @result = "won"
-    elsif computer_play == "Paper"
-      @result = "tied"
-    else
-      @result = "lost"
+    case computer_play
+      when "Rock"
+        @result = "won"
+      when "Paper"
+        @result = "tied"
+      when "Scissors"
+        @result = "lost"
     end
+
+    result_label(@result)
   end
 
   def scissors
-    computer_play
     @player_move = "Scissors"
 
-    if computer_play == "rock"
-      @result = "lost"
-    elsif computer_play == "Paper"
-      @result = "won"
-    else
-      @result = "tied"
+    case computer_play
+      when "Rock"
+        @result = "lost"
+      when "Paper"
+        @result = "won"
+      when "Scissors"
+        @result = "tied"
     end
+
+    result_label(@result)
   end
 
   private
@@ -49,6 +55,17 @@ class GameController < ApplicationController
         @computer_play = "Paper"
       when 3
         @computer_play = "Scissors"
+    end
+  end
+
+  def result_label(result)
+    case result
+      when "won"
+        @result_label = "label-success"
+      when "lost"
+        @result_label = "label-danger"
+      when "tied"
+        @result_label = "label-warning"
     end
   end
 end
